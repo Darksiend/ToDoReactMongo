f59ebbb0eb5d5005e12d513f4226186b53c79d48
Darksiend <logosmk2@gmail.com>
2022-12-03 13:50:50 +0200

    Work

--- front/src/Components/ItemList.jsx
+++ front/src/Components/ItemList.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ItemList.css";
 const ItemList = () => {
+  const [todo, setTodo] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001")
+      .then((res) => res.json())
+      .then((res) => setTodo(res));
+  }, []);
   return (
     <div className="main">
       <div className="ItemList">
         <div className="logo">ToDos</div>
+        <input className="input" type="text" />
+        <input className="input" type="text" />
+        <button className="btn">Add ToDo!</button>
+        {todo.map((item) => {
+          return (
+            <div>
+              <div className="todoName">{item.todoName}</div>
+              <div className="todoDescription">{item.todoDescription}</div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
